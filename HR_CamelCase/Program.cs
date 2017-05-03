215d22fbcf3aa840aa563061f1c2b8d083876b11
Дмитрий Смищенко <dimonovic@gmail.com>
2017-05-03 23:14:34 +0300

    added solution CamelCase

---
+++ HR_CamelCase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int count;
+            if (s[0] < 97)
+                count = 0;
+            else
+                count = 1;
+
+
+            foreach (char item in s)
+            {
+
+                if ((int)item<97)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
