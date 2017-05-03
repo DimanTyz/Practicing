c54895099db630c8f7799af749a6cdacd1235da9
Дмитрий Смищенко <dimonovic@gmail.com>
2017-05-03 23:06:09 +0300

    added soution for reduced strings hr

--- HR_SuperReducedString/Program.cs
+++ HR_SuperReducedString/Program.cs
@@ -10,7 +10,44 @@ namespace HR_SuperReducedString
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
 
+            bool flag = true;
+            while (flag)
+            {
+                flag = Operation(ref s);
+            }
+
+            Console.WriteLine(string.IsNullOrEmpty(s)? "Empty String" : s);
+
+            Console.ReadKey();
+        }
+
+        public static bool Operation(ref string s)
+        {
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] == s[i+1])
+                {
+                    s = s.Replace(new string(s[i],2), "  ");
+                    //s = s.Replace(s[i+1], ' ');
+                    s = RemoveSpace(s);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static string RemoveSpace(string s)
+        {
+            string temp = "";
+            foreach (var item in s)
+            {
+                if(item!=' ')
+                temp += item;
+            }
+            return temp;
         }
     }
 }
